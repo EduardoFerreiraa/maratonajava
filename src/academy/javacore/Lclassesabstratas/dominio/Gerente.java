07b89dfd1ace55100f016f29601f2b47fb2ed448
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-01-12 18:47:29 -0300

    Criei uma classe abstrata e estendi

---
+++ src/academy/javacore/Lclassesabstratas/dominio/Gerente.java
@@ -0,0 +1,12 @@
+package academy.javacore.Lclassesabstratas.dominio;
+
+public class Gerente extends Funcionario {
+	public Gerente(String nome, double salario) {
+		super(nome, salario);
+	}
+	
+	@Override
+	public String toString() {
+		return "Gerente [nome=" + nome + ", salario=" + salario + "]";
+	}
+}
---
+++ src/academy/javacore/Lclassesabstratas/test/FuncionarioTest01.java
@@ -0,0 +1,16 @@
+package academy.javacore.Lclassesabstratas.test;
+
+import academy.javacore.Lclassesabstratas.dominio.Desenvolvedor;
+import academy.javacore.Lclassesabstratas.dominio.Funcionario;
+import academy.javacore.Lclassesabstratas.dominio.Gerente;
+
+public class FuncionarioTest01 {
+	public static void main(String[] args) {
+		Gerente gerente  = new Gerente("Nami", 5000);
+		Desenvolvedor desenvolvedor = new Desenvolvedor("Eduardo", 2500);
+		System.out.println(gerente);
+		System.out.println(desenvolvedor);
+
+	}
+
+}
