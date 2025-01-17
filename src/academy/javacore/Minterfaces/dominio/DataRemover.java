5fe823b199e135149df9f87a0360ef92fbfdaa62
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-01-16 23:55:41 -0300

    Implementando múltiplas interfaces

---
+++ src/academy/javacore/Minterfaces/dominio/DataRemover.java
@@ -0,0 +1,5 @@
+package academy.javacore.Minterfaces.dominio;
+
+public interface DataRemover {
+	public abstract void remove();
+}
---
+++ src/academy/javacore/Minterfaces/test/DataLoaderTest01.java
@@ -0,0 +1,20 @@
+package academy.javacore.Minterfaces.test;
+
+import academy.javacore.Minterfaces.dominio.DatabaseLoader;
+import academy.javacore.Minterfaces.dominio.FileLoader;
+
+public class DataLoaderTest01 {
+	public static void main(String[] args) {
+		DatabaseLoader databaseLoader = new DatabaseLoader();
+		FileLoader fileloader = new FileLoader();
+		databaseLoader.load();
+		fileloader.load();
+		
+		databaseLoader.remove();
+		fileloader.remove();
+		
+		databaseLoader.checkPermission();
+		fileloader.checkPermission();
+	}
+
+}
