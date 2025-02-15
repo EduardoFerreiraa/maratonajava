e30390127ddd844fc5048ac8b5cc0d65deb084b8
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-02-15 11:06:17 -0300

    Internacionalização de Números

--- src/academy/javacore/Sformatacao/test/LocaleTest01.java
+++ src/academy/javacore/Sformatacao/test/LocaleTest01.java
@@ -1,4 +1,4 @@
-package academy.javacore.Rdatas.test;
+package academy.javacore.Sformatacao.test;
 
 import java.text.DateFormat;
 import java.util.Calendar;
---
+++ src/academy/javacore/Sformatacao/test/LocaleTest02.java
@@ -0,0 +1,23 @@
+package academy.javacore.Sformatacao.test;
+
+import java.util.Locale;
+
+public class LocaleTest02 {
+	public static void main(String[] args) {
+		System.out.println(Locale.getDefault());
+		String[] isoCountries = Locale.getISOCountries();
+		String[] isoLanguages = Locale.getISOLanguages();
+		
+		for (String isoLanguage : isoLanguages) {
+			System.out.print(isoLanguages+" ");
+
+		}
+		System.out.println();
+		for (String isoCountry : isoLanguages) {
+			System.out.print(isoCountry+" ");
+
+		}
+
+	}
+
+}
