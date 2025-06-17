9c45dcd65f2db4210956a40156340f6111ccee55
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-06-16 22:05:33 -0300

    Trabalhando com Pattern e Matcher

--- src/academy/javacore/Tresourcebundle/test/ResourceBundleTest01.java
+++ src/academy/javacore/Tresourcebundle/test/ResourceBundleTest01.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class ResourceBundleTest01 {
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		System.out.println(Locale.getDefault());
 		ResourceBundle bundle = ResourceBundle.getBundle("messages", new Locale("en", "US"));
