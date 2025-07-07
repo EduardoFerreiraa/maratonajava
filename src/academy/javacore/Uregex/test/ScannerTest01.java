9291774dc2f207b7c425d0e4d518941ba39158f7
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-07-07 20:45:11 -0300

    Update ScannerTest01.java

--- src/academy/javacore/Uregex/test/ScannerTest01.java
+++ src/academy/javacore/Uregex/test/ScannerTest01.java
@@ -2,7 +2,7 @@ package academy.javacore.Uregex.test;
 
 public class ScannerTest01 {
 	public static void main(String[] args) {
-		String texto = "Levi, Eren, Mikasa";
+		String texto = "Levi, Eren, Mikasa, true, 200";
 		String[] nomes = texto.split(",");
 		
 		for (String nome : nomes) {
