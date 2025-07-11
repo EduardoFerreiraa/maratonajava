afaf0d677b5034a8df1029fdf97e0857872b21f2
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-07-10 21:43:07 -0300

    Lendo arquivo com array

--- src/academy/javacore/Vio/test/FileReaderTest01.java
+++ src/academy/javacore/Vio/test/FileReaderTest01.java
@@ -10,7 +10,20 @@ public class FileReaderTest01 {
 		File file = new File("filee.txt");
 		
 		try (FileReader fr = new FileReader(file)) {
+			// char[] in = new char[30];
+			// fr.read(in);
+			// 
+			// for (char c : in) {
+			// 	System.out.print(c);
+			// }
 			
+			int i;
+			
+			while((i=fr.read()) != -1) {
+				System.out.print((char)i);
+
+			}
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
