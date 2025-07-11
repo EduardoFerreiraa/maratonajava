2722efb8c01c9e9e929c393db260a0f14b9cf006
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-07-10 21:29:45 -0300

    Crie o Try Catch com resources

--- src/academy/javacore/Vio/test/FileReaderTest01.java
+++ src/academy/javacore/Vio/test/FileReaderTest01.java
@@ -1,7 +1,20 @@
 package academy.javacore.Vio.test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileReaderTest01 {
 	public static void main(String[] args) {
+		File file = new File("filee.txt");
+		
+		try (FileReader fr = new FileReader(file)) {
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		
 	}
 
