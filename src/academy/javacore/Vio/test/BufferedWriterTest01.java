b598454f8fa017c3e8af7e519f269f3434938894
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-07-10 22:03:26 -0300

    Usando BufferWriter dentro de FileWriter

--- src/academy/javacore/Vio/test/BufferedWriterTest01.java
+++ src/academy/javacore/Vio/test/BufferedWriterTest01.java
@@ -1,8 +1,20 @@
 package academy.javacore.Vio.test;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class BufferedWriterTest01 {
 	public static void main(String[] args) {
-		
+		File file = new File("filee.txt");
+		try (FileWriter fileWriter = new FileWriter(file, true)) {
+			BufferedWriter bufferWriter = new BufferedWriter(fileWriter);
+			fileWriter.write("O Dev dojo é o melhor youtuber do Brasilllll\n Continuando a cantoria na próxima linha");
+			fileWriter.flush();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 	}
 
 }
