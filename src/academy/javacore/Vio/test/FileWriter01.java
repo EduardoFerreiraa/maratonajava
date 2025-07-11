8e221b2d21d3f2d23d7d9086aad12119f29b9b0c
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-07-10 21:15:30 -0300

    Escrevendo texto em arquivos com FileWriter

--- src/academy/javacore/Vio/test/FileWriter01.java
+++ src/academy/javacore/Vio/test/FileWriter01.java
@@ -1,13 +1,24 @@
 package academy.javacore.Vio.test;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 // File
 // FileWriter
 // FileReader
 // BufferedWriter
-// 
+// BufferedReader
+
 public class FileWriter01 {
 	public static void main(String[] args) {
-		
+		File file = new File("filee.txt");
+		try (FileWriter fileWriter = new FileWriter(file, true)) {
+			fileWriter.write("O Dev dojo é o melhor youtuber do Brasilllll\n Continuando a cantoria na próxima linha");
+			fileWriter.flush();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 	}
 
 }
