8ee9125e3232c0c9ff3c884e6c5caee786cdf2a3
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-07-10 22:08:06 -0300

    Adicionando BufferWriter no lugar de FileWriter

--- src/academy/javacore/Vio/test/BufferedWriterTest01.java
+++ src/academy/javacore/Vio/test/BufferedWriterTest01.java
@@ -10,8 +10,8 @@ public class BufferedWriterTest01 {
 		File file = new File("filee.txt");
 		try (FileWriter fileWriter = new FileWriter(file, true)) {
 			BufferedWriter bufferWriter = new BufferedWriter(fileWriter);
-			fileWriter.write("O Dev dojo é o melhor youtuber do Brasilllll\n Continuando a cantoria na próxima linha");
-			fileWriter.flush();
+			bufferWriter.write("O Dev dojo é o melhor youtuber do Brasilllll");
+			bufferWriter.flush();
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
