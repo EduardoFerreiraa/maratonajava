6075f3dee4f650aab57f51697717bdcd34c7be42
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-08-25 20:14:02 -0300

    Criei um diretorio

--- src/academy/javacore/Vio/test/FileTest02.java
+++ src/academy/javacore/Vio/test/FileTest02.java
@@ -4,7 +4,10 @@ import java.io.File;
 
 public class FileTest02 {
 	public static void main(String[] args) {
-		File fileDiretorio = new File("")
+		File fileDiretorio = new File("pasta");
+		boolean isDiretorioCriado = fileDiretorio.mkdir();
+		System.out.println(isDiretorioCriado);
+		
 	}
 
 }
