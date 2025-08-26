24ae46dcb152f620b6d83debcf5c1d469cf48642
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-08-26 16:38:09 -0300

    Criando uma pasta, renomeando o arquivo e a pasta.

--- src/academy/javacore/Vio/test/FileTest02.java
+++ src/academy/javacore/Vio/test/FileTest02.java
@@ -7,17 +7,20 @@ public class FileTest02 {
 	public static void main(String[] args) throws IOException {
 		File fileDiretorio = new File("pasta");
 		boolean isDiretorioCreated = fileDiretorio.mkdir();
-		
-		System.out.println(isDiretorioCreated);
+		System.out.println("Pasta criada? " + isDiretorioCreated);
 		
 		File fileArquivoDiretorio = new File(fileDiretorio, "arquivo.txt");
 		boolean isFileCreated = fileArquivoDiretorio.createNewFile();
-		System.out.println(isFileCreated);
+		System.out.println("Arquivo criado? " + isFileCreated);
 
-		File fileRenamed = new File("arquivo_renomeado.txt");
+		File fileRenamed = new File(fileDiretorio, "arquivo_renomeado.txt");
 		boolean isRenamed = fileArquivoDiretorio.renameTo(fileRenamed);
+		System.out.println("Arquivo renomeado? " + isRenamed);
 		
-		System.out.println(isRenamed);
+		File diretorioRenamed = new File("pasta2");
+		boolean isDiretorioRenomeado = fileDiretorio.renameTo(diretorioRenamed);
+		System.out.println("Diretorio pasta renomeada? " + isDiretorioRenomeado);
+
 
 
 
