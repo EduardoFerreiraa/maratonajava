f1be8db5fa9cd50c877a3c4764782770460867ff
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-08-31 21:56:46 -0300

    Criei a classe DosFileAttributes

---
+++ src/academy/javacore/Wnio/test/DosFileAttributesTest01.java
@@ -0,0 +1,13 @@
+package academy.javacore.Wnio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DosFileAttributesTest01 {
+	public static void main(String[] args) {
+		Path path = Paths.get("pasta/teste.txt");
+		
+		
+	}
+
+}
