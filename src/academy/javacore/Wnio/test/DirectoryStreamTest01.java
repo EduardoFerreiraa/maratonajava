cbe29c7ae11b07099f0d8f2b72e572a5d4644a51
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-09-01 20:45:10 -0300

    Chamei a classe DirectoryStream

--- src/academy/javacore/Wnio/test/DirectoryStreamTest01.java
+++ src/academy/javacore/Wnio/test/DirectoryStreamTest01.java
@@ -1,8 +1,19 @@
 package academy.javacore.Wnio.test;
 
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class DirectoryStreamTest01 {
 	public static void main(String[] args) {
-		
+		Path path = Paths.get("/maratonajava");
+		try(DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
