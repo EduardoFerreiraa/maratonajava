73d7ff4e6592c480c2daa287e3b74907f35f8d45
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-09-01 22:12:40 -0300

    Chamei a classe Path e walkFileTree

--- src/academy/javacore/Wnio/test/SimpleFileVisitorTest01.java
+++ src/academy/javacore/Wnio/test/SimpleFileVisitorTest01.java
@@ -1,8 +1,13 @@
 package academy.javacore.Wnio.test;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class SimpleFileVisitorTest01 {
 	public static void main(String[] args) {
-		
+		Path path = Paths.get(".");
+		Files.walkFileTree(null, null);
 	}
 
 }
