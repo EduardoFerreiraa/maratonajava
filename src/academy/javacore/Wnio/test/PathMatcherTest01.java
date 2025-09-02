cc13b9d7ca9650a553c3c70a77c6fb2f24bf02a7
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-09-02 10:51:56 -0300

    Update PathMatcherTest01

--- src/academy/javacore/Wnio/test/PathMatcherTest01.java
+++ src/academy/javacore/Wnio/test/PathMatcherTest01.java
@@ -1,8 +1,17 @@
 package academy.javacore.Wnio.test;
 
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+
 public class PathMatcherTest01 {
 	public static void main(String[] args) {
 		
+	}
+	
+	private static void matches(Path path, String glob) {
+		PathMatcher matcher = FileSystems.getDefault().getPathMatcher(glob);
+		System.out.println(glob + ": " + matcher.matches(path));
 	}
 
 }
