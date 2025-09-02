5a11d3a3962c38e6c2c4868c1eb30806d62b0567
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-09-02 10:22:18 -0300

    Rename to "ListJavaFiles"

--- src/academy/javacore/Wnio/test/SimpleFileVisitorTest02.java
+++ src/academy/javacore/Wnio/test/SimpleFileVisitorTest02.java
@@ -8,7 +8,7 @@ import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
-class ListJavaFiles extends SimpleFileVisitor<Path> {
+class ListAllFiles extends SimpleFileVisitor<Path> {
 
 	@Override
 	public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
@@ -22,7 +22,7 @@ class ListJavaFiles extends SimpleFileVisitor<Path> {
 	
 }
 
-public class SimpleFileVisitorTest01 {
+public class SimpleFileVisitorTest02 {
 	public static void main(String[] args) throws IOException {
 		Path root = Paths.get(".");
 		Files.walkFileTree(root, new ListAllFiles());
