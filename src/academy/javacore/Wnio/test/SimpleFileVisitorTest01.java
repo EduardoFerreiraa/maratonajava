1c15b7ddeea16964e16840862600fdc0cd2c26c4
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-09-01 22:16:45 -0300

    Implementando um visitFile

--- src/academy/javacore/Wnio/test/SimpleFileVisitorTest01.java
+++ src/academy/javacore/Wnio/test/SimpleFileVisitorTest01.java
@@ -1,13 +1,26 @@
 package academy.javacore.Wnio.test;
 
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+class ListAllFiles extends SimpleFileVisitor<Path> {
+
+	@Override
+	public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+		
+	}
+	
+}
 
 public class SimpleFileVisitorTest01 {
 	public static void main(String[] args) {
 		Path path = Paths.get(".");
-		Files.walkFileTree(null, null);
+		Files.walkFileTree(root, );
 	}
 
 }
